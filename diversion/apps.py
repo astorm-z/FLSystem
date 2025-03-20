43b2ff45f1e8c1ea859c7f5bf5a18603b62e1c65
astorm <forczc@foxmail.com>
2025-03-20 15:06:54 +0800

    1

--- diversion/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiversionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diversion'
-    verbose_name = '分流系统' 
